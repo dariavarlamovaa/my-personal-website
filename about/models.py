c2e85ac35b0d11ac46f7fd5e496a2a4e4ec63b8c
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-08-28 21:00:43 +0300

    updated

--- about/models.py
+++ about/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    title = models.CharField(max_length=250)
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
--- about/views.py
+++ about/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from home.models import Menu
+from .models import Skill
 
 
 def about(request):
     menu = Menu.objects.all()
-    return render(request, 'about/about.html', {'menu': menu})
+    skills = Skill.objects.all()
+    return render(request, 'about/about.html', {'menu': menu, 'skills': skills})
